a9f000252656ae1cae400f23a7797a9e21dbb63c
QQ <QQ@DESKTOP-FIEFGKN>
2018-05-13 14:27:20 +0700

    Created by Miftakhuddin Falki v2.0

--- src/Hitung.java
+++ src/Hitung.java
@@ -50,17 +50,36 @@ public class Hitung {
 //            System.out.println(nilai[i]);
             }
     }
+    public static void arrayduadimensi(){
+    int [][] piksel = new int [2][3];
+    piksel[0][0] = 70;
+    piksel[0][1] = 18;
+    piksel[0][2] = 45;
+    piksel[1][0] = 75;
+    piksel[1][1] = 66;
+    piksel[1][2] = 89;
+    
+    
+    int i,j;
+    int k = 0;
+    for(i=0;i<2;i++){
+        for (j=0;j<3;j++)
+                k = piksel[i][j] + piksel [i ][j];
+            System.out.print(k + " ");
+        System.out.println("");
+    }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        dectohex();
+//        dectohex();
 //        Hitung converter = new Hitung();
 //            Hitung.dectohex(12);
       
 //                array();
-                
+                arrayduadimensi();
                 }
     
 }
